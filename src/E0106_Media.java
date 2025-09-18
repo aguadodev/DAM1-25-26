d24fe57ef29ea902432ca2aa792c3e6a5e482adb
aguadodev <aguado@edu.xunta.es>
2025-09-18 14:27:26 +0200

    Ejercicio media

---
+++ src/E0106_Media.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+/*
+ * E0106_Media. Crea una aplicación que calcule la media aritmética de tres notas enteras. Ten en cuenta que la media puede tener decimales. 
+ */
+
+ /**
+  * @Author Marco
+  */
+public class E0106_Media {
+    public static void main(String[] args) {
+        byte num1;
+        byte num2;
+        byte num3;
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Dame la primera nota: ");
+        num1 = sc.nextByte();
+
+        System.out.print("Dame la segunda nota: ");
+        num2 = sc.nextByte();
+
+        System.out.print("Dame la tercera nota: ");
+        num3 = sc.nextByte();
+        
+        sc.close();
+
+        double suma = num1 + num2 + num3;
+        double resultado = suma / 3;
+
+        System.out.println("tu media es de " + resultado);
+    }
+}
