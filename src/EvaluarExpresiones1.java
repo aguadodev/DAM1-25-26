633937a113acc9373a5a4923476c15035730e4c1
aguadodev <aguado@edu.xunta.es>
2025-09-23 14:28:05 +0200

    Expresiones y Operadores

---
+++ src/EvaluarExpresiones1.java
@@ -0,0 +1,13 @@
+public class EvaluarExpresiones1 {
+    public static void main(String[] args) {
+        int a = 8, b = 3, c = -5;
+
+        System.out.println("a + b + c = " + (a + b + c));
+        System.out.println("2 * b + 3 * (a - c) = " + (2 * b + 3 * (a - c)));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+        System.out.println("a / c = " + (a / c));
+        System.out.println("a % c = " + (a % c));
+
+    }
+}
---
+++ src/EvaluarExpresiones2.java
@@ -0,0 +1,26 @@
+public class EvaluarExpresiones2 {
+    public static void main(String[] args) {
+        int resultado = 10;
+        resultado += 5 - 3 * 2;
+
+        boolean resultado1 = 4 + 5 > 10 - 3;
+
+        boolean resultado2 = 5 > 3 && 8 < 6 || 3 == 3;
+
+        int resultado3 = (4 + 3) * 2 - 6 / 3;
+
+        int a = 10, b = 20;
+        int resultado4 = a > b ? a : b;
+
+        int x = 5;
+        int resultado5 = ++x * 2;
+
+    System.out.println("Resultado = " + resultado);        
+    System.out.println("Resultado1 = " + resultado1);        
+    System.out.println("Resultado2 = " + resultado2);        
+    System.out.println("Resultado3 = " + resultado3);        
+    System.out.println("Resultado4 = " + resultado4);        
+    System.out.println("Resultado5 = " + resultado5);        
+
+    }
+}
