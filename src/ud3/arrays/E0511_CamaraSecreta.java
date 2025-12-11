c9485c87523c3cf4ef175d8eb3411b63380bb79c
aguadodev <aguado@edu.xunta.es>
2025-12-11 14:15:43 +0100

    Arrays

---
+++ src/ud3/arrays/E0511_CamaraSecreta.java
@@ -0,0 +1,10 @@
+package ud3.arrays;
+
+import java.util.Scanner;
+
+public class E0511_CamaraSecreta {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
--- src/ud3/arrays/Util.java
+++ src/ud3/arrays/Util.java
@@ -3,6 +3,8 @@ package ud3.arrays;
 import java.util.Arrays;
 import java.util.Random;
 
+import javax.swing.JOptionPane;
+
 public class Util {
 
     static public int[] insertarAlFinal(int[] t, int num) {
@@ -43,6 +45,14 @@ public class Util {
         return tRes;
     }
 
+    static public int[] borrarOrdenado(int[] t, int num) {
+        int pos = Arrays.binarySearch(t, num);
+        if (pos != -1) {
+            return eliminarEnPosicion(t, pos);
+        }
+        return t;
+    }
+
     public static void desordenar(int t[]) {
         Random rnd = new Random();
         // Recorrer cada elemento
@@ -56,7 +66,21 @@ public class Util {
         }
     }
 
-    static int[] copiaDesordenada(int[] tOrdenada){
+
+    // Métodos Interfaz gráfica
+    public static int ventanaLeerInt(String prompt) {
+        return Integer.parseInt(JOptionPane.showInputDialog(prompt));
+    }
+
+    public static String ventanaLeer(String prompt) {
+        return JOptionPane.showInputDialog(prompt);
+    }
+
+    public static void ventanaImprimir(String str) {
+        JOptionPane.showMessageDialog(null, str);        
+    }
+
+    public static int[] copiaDesordenada(int[] tOrdenada) {
         int[] t = tOrdenada.clone();
         Random rnd = new Random();
         // Recorrer cada elemento
@@ -67,29 +91,35 @@ public class Util {
             int aux = t[i];
             t[i] = t[posRnd];
             t[posRnd] = aux;
-        }         
+        }
         return t;
     }
 
     public static void main(String[] args) {
-        int[] numeros = { 1, 2, 3, 4, 5, 6, 7};
-        //numeros = insertarAlFinal(numeros, 5);
-        //numeros = insertarAlInicio(numeros, 5);
-        //numeros = insertarEnPosicion(numeros, 5, 2);
-        //numeros = eliminarAlFinal(numeros);
-        //int[] menosNumeros = eliminarAlInicio(numeros);
+        /*
+        int num = ventanaLeerInt("Introduce el dato: ");
+        ventanaImprimir("Valor:" + num);
+
+        int[] numeros = { 1, 2, 3, 4, 5, 6, 7 };
+        System.out.println(Arrays.toString(borrarOrdenado(numeros, 5)));
+        
+        // numeros = insertarAlFinal(numeros, 5);
+        // numeros = insertarAlInicio(numeros, 5);
+        // numeros = insertarEnPosicion(numeros, 5, 2);
+        // numeros = eliminarAlFinal(numeros);
+        // int[] menosNumeros = eliminarAlInicio(numeros);
         int[] menosNumeros = eliminarEnPosicion(numeros, 2);
         System.out.println(Arrays.toString(menosNumeros));
-        System.out.println(Arrays.toString(numeros));
+        System.out.println(Arrays.toString(numeros)); */
 
-        /*
-        int t1[] = {1, 2, 3};
-        int t2[] = {1, 2, 3};
         
-        System.out.println(t1 == t2); // false
-        
-        System.out.println(Arrays.equals(t1, t2)); // true
-         */
+         int t1[] = {1, 2, 3};
+         int t2[] = {1, 2, 3};
+         
+         System.out.println(t1 == t2); // false
+          
+         System.out.println(Arrays.equals(t1, t2)); // true
+         
     }
 
 }
