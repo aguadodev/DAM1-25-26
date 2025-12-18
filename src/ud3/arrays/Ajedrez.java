c8a54167cb1c9aba8ebb19349237921f8b9763b4
aguadodev <aguado@edu.xunta.es>
2025-12-18 14:26:05 +0100

    Ajedrez

--- src/ud3/arrays/Ajedrez.java
+++ src/ud3/arrays/Ajedrez.java
@@ -12,40 +12,68 @@ public class Ajedrez {
         System.out.println(turnoBlancas ? "Turno de BLANCAS" : "Turno de NEGRAS");
         int[] mov = leerMovimiento();
         while (!fin) {
-            //validarMovimiento
+            // validarMovimiento
             mover(tablero, mov);
-            //comprobarJaqueOJaqueMate
+            turnoBlancas = !turnoBlancas;
+            // comprobarJaqueOJaqueMate
             mostrarTableroConLeyenda(tablero);
+            System.out.println(turnoBlancas ? "Turno de BLANCAS" : "Turno de NEGRAS");
             mov = leerMovimiento();
         }
-        
+
         // Mensaje final: ganador/a o tablas
 
         System.out.println("Fin de la partida!");
     }
 
     private static void mover(char[][] tablero, int[] mov) {
-        
+        tablero[mov[2]][mov[3]] = tablero[mov[0]][mov[1]];
+        tablero[mov[0]][mov[1]] = '-';
     }
 
     private static int[] leerMovimiento() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Movimiento? Ejemplo \"e2 e4\": ");
-        String entrada = sc.nextLine();
-        char columnaOrigen = entrada.charAt(0); // columnaOrigen e
-        char filaOrigen = entrada.charAt(1); // filaOrigen 2
-        char columnaDestino = entrada.charAt(3); // columnaDestino e
-        char filaDestino = entrada.charAt(4); // filaDestino 4
+        Boolean entradaValida = false;
+        int[] movimiento = null;
+        do {
+            System.out.print("Movimiento? Ejemplo \"e2 e4\"  o  \"fin\" para rendirte: ");
+            String entrada = sc.nextLine();
+            if (entrada.equalsIgnoreCase("fin"))
+                return null;
 
-        int[] movimiento = {
-            7 - (filaOrigen - '1'),            
-            columnaOrigen - 'a',
-            7 - (filaDestino - '1'),
-            columnaDestino - 'a'
-        };
+            try {
+                char columnaOrigen = entrada.charAt(0); // columnaOrigen e
+                char filaOrigen = entrada.charAt(1); // filaOrigen 2
+                char columnaDestino = entrada.charAt(3); // columnaDestino e
+                char filaDestino = entrada.charAt(4); // filaDestino 4
+
+                int cOrigen = columnaOrigen - 'a';
+                int fOrigen = 7 - (filaOrigen - '1');
+                int cDestino = columnaDestino - 'a';
+                int fDestino = 7 - (filaDestino - '1');
+                
+                if (cOrigen >= 0 && cOrigen < 8 
+                    && fOrigen >= 0 && fOrigen < 8 
+                    && cDestino >= 0 && cDestino < 8 
+                    && fDestino >= 0 && fDestino < 8 
+                )
+                
+                movimiento = new int[4];
+                movimiento[0] = fOrigen;
+                movimiento[1] = cOrigen;
+                movimiento[2] = fDestino;
+                movimiento[3] = cDestino;
+                
+                entradaValida = true;
+    
+            } catch (Exception e) {
+                System.out.println("Entrada inválida");
+            }
+
+
+        } while (!entradaValida);        
         return movimiento;
     }
-
 
     static char[][] inicializarTablero() {
         char[][] nuevoTablero = {
@@ -63,7 +91,7 @@ public class Ajedrez {
     }
 
     static void mostrarTableroConLeyenda(char[][] t) {
-        System.out.println();        
+        System.out.println();
 
         System.out.println("    a b c d e f g h");
         System.out.println("    ---------------");
@@ -79,28 +107,9 @@ public class Ajedrez {
         }
         System.out.println("    ---------------");
         System.out.println("    a b c d e f g h");
-        
-        System.out.println();        
+
+        System.out.println();
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     /*
      * MÉTODOS ADICIONALES
