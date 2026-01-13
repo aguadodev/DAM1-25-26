c04045f64062fabc8bbca33762bfcede1121dc5c
aguadodev <aguado@edu.xunta.es>
2026-01-13 14:30:37 +0100

    ContarOcurrencias

--- src/ud3/strings/E0607.java
+++ src/ud3/strings/E0607.java
@@ -8,15 +8,22 @@ public class E0607 {
                 En la actualidad, la noción de programación se encuentra muy asociada a la creación de aplicaciones de informática y videojuegos. En este sentido, es el proceso por el cual una persona desarrolla un programa, valiéndose de una herramienta que le permita escribir el código (el cual puede estar en uno o varios lenguajes, como C++, Java y Python, entre muchos otros) y de otra que sea capaz de “traducirlo” a lo que se conoce como lenguaje de máquina, que puede "comprender" el microprocesador.[2]​
                                 """;
 
-        System.out.println(contarOcurrencias(texto, "Programación"));
+        System.out.println(contarOcurrencias(texto, "programación")); // 2
+        System.out.println(contarOcurrencias(texto, " el ")); // 9
 
     }
 
     static int contarOcurrencias(String frase, String palabra) {
         int contador = 0;
+        int posicionBusqueda = 0;
 
-        //...
-        
+        while (frase.indexOf(palabra, posicionBusqueda) != -1) {
+            // Incrementar contador
+            contador++;
+            // Incrementar posición de búsqueda
+            posicionBusqueda = frase.indexOf(palabra, posicionBusqueda) + 1;
+        }
+
         return contador;
     }
 }
