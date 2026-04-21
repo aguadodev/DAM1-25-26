6ff22eff14af9e833a8a1f02541bb31adf9858dd
aguadodev <aguado@edu.xunta.es>
2026-04-21 14:31:43 +0200

    E1304

--- src/ud5/E1304.java
+++ src/ud5/E1304.java
@@ -1,18 +1,51 @@
 package ud5;
 
+import java.sql.Array;
+import java.util.Arrays;
+import java.util.Random;
 import java.util.function.Predicate;
 
+/**
+ * Implementar un método estático (filtrar) al que se pasa como parámetro una
+ * tabla de tipo genérico y un predicado. El método devuelve otra tabla con los
+ * elementos de
+ * la tabla original que verifiquen la condición del predicado. Aplicarlo a una
+ * tabla de 50
+ * enteros entre 1 y 100 que devuelve los múltiplos de 3.
+ * Prueba a implementar una versión del método que utilice listas en lugar de
+ * arrays
+ */
 public class E1304 {
+    public static <T> T[] filtrar (T[] t, Predicate<T> p) {
 
-    public static void main(String[] args) {
-        Integer[] numeros;
+        T[] nuevaT = Arrays.copyOf(t, 0);
 
-        Predicate<Integer> esMultiploDe3 = // ...
+        for (T e : t) {
+            if (p.test(e)) {
+                nuevaT = Arrays.copyOf(nuevaT, nuevaT.length + 1);
+                nuevaT[nuevaT.length - 1] = e;
+            }
+        }
 
-        Integer multiplosDe3 = filtrar(numeros, esMultiploDe3);
+        return nuevaT;
     }
+    
+    public static void main(String[] args) {
+        Integer[] numeros = new Integer[50];
 
+        Random rnd = new Random();
 
-    // Método genérico filtrar
+        for (int i = 0; i < numeros.length; i++) {
+            int random = rnd.nextInt(1, 101);
+            numeros[i] = random;    
+        }
 
+        Predicate<Integer> esMultiploDe3 = x -> x % 3 == 0;
+
+        System.out.println(Arrays.toString(numeros));
+
+        Integer[] multiplosDe3 = filtrar(numeros, esMultiploDe3);
+
+        System.out.println(Arrays.toString(multiplosDe3));
+    }
 }
