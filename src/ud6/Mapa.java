3b6204c08b1c7b18e522e996654a6b337800c052
aguadodev <aguado@edu.xunta.es>
2026-05-08 13:44:26 +0200

    JavaFX

--- src/ud6/Mapa.java
+++ src/ud6/Mapa.java
@@ -1,7 +1,12 @@
 package ud6;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
-public class Mapa {
+public class Mapa extends Application{
 
     int[][] mapa = {
         {1, 0, 1, 1, 1, 1},
@@ -11,6 +16,32 @@ public class Mapa {
         {1, 1, 1, 1, 0, 1},
     };
 
-    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+
+        GridPane gridPane = new GridPane();
+
+        for (int i = 0; i < mapa.length; i++) {
+            for (int j = 0; j < mapa[i].length; j++) {
+                Label lbl = new Label();
+                lbl.setOnMouseClicked(e -> lbl.setStyle("-fx-background-color: red;"));
+                if (mapa[i][j] == 1) {
+                    lbl.setStyle("-fx-background-color: navy;");
+                }
+                lbl.setPrefSize(50, 50);
+                gridPane.add(lbl, j, i);
+            }
+        }
+
+
+        Scene scene = new Scene(gridPane);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 
 }
