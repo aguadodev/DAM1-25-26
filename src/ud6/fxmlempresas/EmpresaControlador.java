068eb4cb18da053a965d22824ba82bb5dbb684ae
aguadodev <aguado@edu.xunta.es>
2026-05-14 14:28:55 +0200

    Empresas con MenuBar y FileChooser

--- src/ud6/fxmlempresas/EmpresaControlador.java
+++ src/ud6/fxmlempresas/EmpresaControlador.java
@@ -1,5 +1,8 @@
 package ud6.fxmlempresas;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -8,6 +11,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 
 public class EmpresaControlador implements Initializable {
 
@@ -31,10 +35,9 @@ public class EmpresaControlador implements Initializable {
     void agregar(ActionEvent event) {
         // @TODO Validar valores
         Empresa e = new Empresa(
-            Integer.parseInt(txtId.getText()),
-            txtNombre.getText(),
-            txtWeb.getText()
-        );
+                Integer.parseInt(txtId.getText()),
+                txtNombre.getText(),
+                txtWeb.getText());
         // @TODO Comprobar que la empresa no está en la lista
         lstEmpresas.getItems().add(e);
     }
@@ -43,6 +46,33 @@ public class EmpresaControlador implements Initializable {
     void borrar(ActionEvent event) {
         Empresa e = new Empresa(Integer.parseInt(txtId.getText()));
         lstEmpresas.getItems().remove(e);
+    }
+
+    @FXML
+    void guardar(ActionEvent event) {
+
+        FileChooser chooser = new FileChooser();
+        chooser.setTitle("Selecciona un archivo");
+        chooser.setInitialDirectory(new File("D:\\Oscar\\Programacion\\DAM1-25-26\\"));
+        chooser.setInitialFileName("empresas.txt");
+        // chooser.setSelectedExtensionFilter(null);
+
+        // File file = chooser.showOpenDialog(null);
+        // @TODO anclar a la ventana actual
+        File file = chooser.showSaveDialog(null);
+
+        if (file != null) {
+            try (BufferedWriter out = new BufferedWriter(new FileWriter(file.getAbsolutePath()))) {
+                for (Empresa e : lstEmpresas.getItems()) {
+                    out.write(e.toString());
+                    out.newLine();
+                }
+            } catch (Exception e) {
+
+            }
+
+        }
+
     }
 
     @Override
